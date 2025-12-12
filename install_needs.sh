c6178f9c873e089a0d21a0780b3af3ac9c959b23
root <root@debian-dev.velocedigital.top>
2025-12-12 14:49:05 +0000

    Edit install needs

--- install_needs.sh
+++ install_needs.sh
@@ -1,12 +1,11 @@
 apk add xz wireguard-tools qbittorrent-nox git nano ncurses jq iptables curl
-git clone https://github.com/pia-foss/manual-connections /opt/pia
-mv -r etc opt /
-cd /etc/init.d
-chmod +x microsocks pia-pf pia-port qbittorrent wg-pia
-cd /opt/pia
-chmod +x build.sh build_options.sh chkip.sh install_microsocks.sh install_needs.sh
+cp -r etc opt /
 rc-update add microsocks default
 rc-update add pia-pf default
 rc-update add pia-port default
 rc-update add qbittorrent default
 rc-update add wg-pia default
+/opt/pia/build_options.sh
+/opt/pia/build.sh
+/opt/pia/install_microsocks.sh
+
