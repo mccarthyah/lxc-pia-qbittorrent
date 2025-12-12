94d1bf9f7f226f53addf96a63306fa73702a38dd
mccarthyah <74882617+mccarthyah@users.noreply.github.com>
2025-12-11 21:38:14 -0500

    Update install_needs.sh

--- opt/pia/install_needs.sh
+++ opt/pia/install_needs.sh
@@ -1,2 +1,7 @@
-apk add xz wireguard-tools qbittorrent-nox git nano ncurses jq iptables
+apk add xz wireguard-tools qbittorrent-nox git nano ncurses jq iptables curl
 git clone https://github.com/pia-foss/manual-connections /opt/pia
+mv -r etc opt /
+cd /etc/init.d
+chmod +x microsocks pia-pf pia-port qbittorrent wg-pia
+cd /opt/pia
+chmod +x build.sh build_options.sh chkip.sh install_microsocks.sh install_needs.sh
