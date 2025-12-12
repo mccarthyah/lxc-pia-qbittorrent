8885f780f69dbfb1071ba62dcc0c6830bf9d2110
root <root@vpn-test.velocedigital.top>
2025-12-12 15:55:49 +0000

    added screen

--- install_needs.sh
+++ install_needs.sh
@@ -1,11 +1,12 @@
-apk add xz wireguard-tools qbittorrent-nox git nano ncurses jq iptables curl
+apk add xz screen wireguard-tools qbittorrent-nox git nano ncurses jq iptables curl
 cp -r etc opt /
 rc-update add microsocks default
 rc-update add pia-pf default
 rc-update add pia-port default
 rc-update add qbittorrent default
 rc-update add wg-pia default
-/opt/pia/build_options.sh
-/opt/pia/build.sh
-/opt/pia/install_microsocks.sh
+cd /opt/pia
+./build_options.sh
+./build.sh
+./install_microsocks.sh
 
