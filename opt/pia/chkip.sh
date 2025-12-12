1a57c72dff63dd09016e54cb806031485ea508af
root <root@debian-dev.velocedigital.top>
2025-12-12 14:02:13 +0000

    Restore local files in /opt/pia alongside manual-connections subtree

---
+++ opt/pia/chkip.sh
@@ -0,0 +1,1 @@
+curl -s https://ifconfig.co
---
+++ opt/pia/services.sh
@@ -0,0 +1,4 @@
+cd /etc/init.d
+chmod +x microsocks pia-pf pia-port qbittorrent wg-pia
+cd /opt/pia
+chmod +x build.sh build_options.sh chkip.sh install_microsocks.sh install_needs.sh
