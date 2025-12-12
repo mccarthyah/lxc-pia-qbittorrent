fded6a3486e9089d07040c88ef79e47893ccf456
mccarthyah <74882617+mccarthyah@users.noreply.github.com>
2025-12-12 11:17:07 -0500

    Update install_needs.sh

--- install_needs.sh
+++ install_needs.sh
@@ -5,6 +5,14 @@ rc-update add pia-pf default
 rc-update add pia-port default
 rc-update add qbittorrent default
 rc-update add wg-pia default
+adduser -D -s /bin/ash qbittorrent
+mkdir -p /home/qbittorrent
+mkdir -p /home/qbittorrent/.config
+mkdir -p /home/qbittorrent/.cache
+chown -R qbittorrent:qbittorrent /home/qbittorrent
+
+
+
 cd /opt/pia
 ./install_microsocks.sh
 ./build_options.sh
