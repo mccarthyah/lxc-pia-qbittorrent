c97cede032ccec30443d9a628d261c23cccf095a
mccarthyah <74882617+mccarthyah@users.noreply.github.com>
2025-12-12 12:24:51 -0500

    Update install_needs.sh

--- install_needs.sh
+++ install_needs.sh
@@ -5,11 +5,12 @@ rc-update add pia-pf default
 rc-update add pia-port default
 rc-update add qbittorrent default
 rc-update add wg-pia default
-mkdir -p /home/qbittorrent
-adduser -D -h /home/qbittorrent -s /bin/ash qbittorrent
+
 mkdir -p /home/qbittorrent
 mkdir -p /home/qbittorrent/.config
 mkdir -p /home/qbittorrent/.cache
+adduser -D -h -s /bin/ash qbittorrent
+
 ./qbt_local_auth.sh
 chown -R qbittorrent:qbittorrent /home/qbittorrent
 
