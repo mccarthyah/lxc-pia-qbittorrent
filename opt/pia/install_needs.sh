10c5dae66aa6610e87af2ea9aa33b52e5378f3af
mccarthyah <74882617+mccarthyah@users.noreply.github.com>
2025-12-11 21:39:56 -0500

    Update install_needs.sh

--- opt/pia/install_needs.sh
+++ opt/pia/install_needs.sh
@@ -5,3 +5,8 @@ cd /etc/init.d
 chmod +x microsocks pia-pf pia-port qbittorrent wg-pia
 cd /opt/pia
 chmod +x build.sh build_options.sh chkip.sh install_microsocks.sh install_needs.sh
+rc-update add microsocks default
+rc-update add pia-pf default
+rc-update add pia-port default
+rc-update add qbittorrent default
+rc-update add wg-pia default
