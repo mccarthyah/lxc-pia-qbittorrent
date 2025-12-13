a402711192c50617493c4762ff2f0865e82fc5fe
mccarthyah <74882617+mccarthyah@users.noreply.github.com>
2025-12-12 19:11:41 -0500

    Update install_needs.sh

--- install_needs.sh
+++ install_needs.sh
@@ -3,7 +3,7 @@ apk add xz screen wireguard-tools qbittorrent-nox git nano ncurses jq iptables c
 cp -r etc opt /
 rc-update add microsocks default
 rc-update add pia-pf default
-rc-update add pia-port default
+rc-update add qbittorrent-set-port default
 rc-update add qbittorrent default
 rc-update add wg-pia default
 
